872a5d7b9dde8a6c57c19194507ff4d1f2d268b7
Adam Baranek <adam.baranek@ci-group.org>
2019-06-20 17:09:17 -0400

    Finished the Number Game! Just needs CSS and any other cool functionalities I want the game to include such as a leaderboard

--- numberGame.js
+++ numberGame.js
@@ -2,6 +2,8 @@ var duration = 0;
 var now = 0;
 var numString = [];
 var stopTrigger = 0;
+var userInput = '';
+var finalScore = 0;
 
 function numbersTimer(value) {
 	if (value > 31) {
@@ -27,18 +29,19 @@ function countTimer() {
 	else {
 		clearInterval(timer); 
 		document.getElementById('numCounter').innerHTML = 'Time is up!';
-		document.getElementById('numDisplay').innerHTML = '';
 
 		if (stopTrigger == 0) {
 			stopTrigger++
-			document.getElementById('numCounter').innerHTML = 'Start Answering!';
+			document.getElementById('numCounter').innerHTML = 'Answer!';
 			numbersTimer(31);
-			//Another function needs to run here to force user to start inputing answers
+			document.getElementById('numDisplay').innerHTML = 
+			'<input type="text" maxlength="20" id="userAnswer">';
 		}
 		else {
 			clearInterval(timer); 
 			document.getElementById('numCounter').innerHTML = 'Time is up!';
-			//Another function to display score and other stats
+			userInput = document.getElementById('userAnswer').value;
+			numAnswer();//Another function to display score and other stats
 		}
 	}
 }
@@ -46,7 +49,6 @@ function countTimer() {
 
 
 function numMemory() {
-	
 	for (var i = 0; i < 20; i++) {
 		numString.push(Math.floor(Math.random() * 9));
 	}
@@ -55,7 +57,21 @@ function numMemory() {
 }
 
 function numAnswer() {
+	var userArray = userInput.split('');
+	document.getElementById('numDisplay2').innerHTML = '';
 
-	document.getElementById('numDisplay').innerHTML = ;
+	for (var i = 0; i < 20; i++) {
+		if (numString[i] == userArray[i]) {
+			finalScore++;
+			document.getElementById('numDisplay2').innerHTML += '<li style="color: green;">' + 
+			userArray[i] + '</li>' + '-';
+		}
+		else {
+			document.getElementById('numDisplay2').innerHTML += '<li style="color: red;">' + 
+			userArray[i] + '</li>' + '-';
+		}
+	}
 
+	document.getElementById('numDisplay2').innerHTML += '<li style="color: blue;">' + 
+	'Final Score: ' + finalScore + '</li>';
 }
