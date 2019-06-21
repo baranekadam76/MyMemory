3c9fe82ef2f7f8ddcab1da39692afa1adcd86553
Adam Baranek <adam.baranek@ci-group.org>
2019-06-21 14:34:21 -0400

    added some little touches to numberGame

--- numberGame.js
+++ numberGame.js
@@ -6,7 +6,7 @@ var userInput = '';
 var finalScore = 0;
 
 function numbersTimer(value) {
-	if (value > 31) {
+	if (value > 60) {
 		numMemory();
 		now = value;
 		timer = setInterval(countTimer, 1000);
@@ -33,9 +33,9 @@ function countTimer() {
 		if (stopTrigger == 0) {
 			stopTrigger++
 			document.getElementById('numCounter').innerHTML = 'Answer!';
-			numbersTimer(31);
+			numbersTimer(60);
 			document.getElementById('numDisplay').innerHTML = 
-			'<input type="text" maxlength="20" id="userAnswer">';
+			'<input type="text" maxlength="30" id="userAnswer">';
 		}
 		else {
 			clearInterval(timer); 
@@ -49,7 +49,7 @@ function countTimer() {
 
 
 function numMemory() {
-	for (var i = 0; i < 20; i++) {
+	for (var i = 0; i < 30; i++) {
 		numString.push(Math.floor(Math.random() * 9));
 	}
 
@@ -60,7 +60,7 @@ function numAnswer() {
 	var userArray = userInput.split('');
 	document.getElementById('numDisplay2').innerHTML = '';
 
-	for (var i = 0; i < 20; i++) {
+	for (var i = 0; i < 30; i++) {
 		if (numString[i] == userArray[i]) {
 			finalScore++;
 			document.getElementById('numDisplay2').innerHTML += '<li style="color: green;">' + 
@@ -73,5 +73,30 @@ function numAnswer() {
 	}
 
 	document.getElementById('numDisplay2').innerHTML += '<li style="color: blue;">' + 
-	'Final Score: ' + finalScore + '</li>';
+	' Final Score: ' + finalScore + '/30</li>' + ' (To start a new game, refresh the page!)';
+
+	switch (finalScore) {
+		case 30:
+			document.getElementById('numCounter').innerHTML = 'My god...';
+			break;
+		case 29:
+		case 28:
+		case 27:
+		case 26:
+			document.getElementById('numCounter').innerHTML = 'You\'re so close!';
+			break;
+	}
+
+	if (finalScore <= 25 && finalScore >= 20) {
+		document.getElementById('numCounter').innerHTML = 'You did very well!';
+	}
+	else if (finalScore <= 19 && finalScore >= 15) {
+		document.getElementById('numCounter').innerHTML = 'Not bad honestly.';
+	}
+	else if (finalScore <= 14 && finalScore >= 9) {
+		document.getElementById('numCounter').innerHTML = 'You can do better!';
+	}
+	else {
+		document.getElementById('numCounter').innerHTML = 'Umm.. How can I put this..';
+	}
 }
